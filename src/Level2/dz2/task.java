8adfea787fdb3f3e1d05e2b87c0955bbae87119f
pp957 <pp9575@yandex.ru>
2022-08-30 10:34:32 +0300

    DZ2, all done, task 3 bad (

--- src/Level2/dz2/task.java
+++ src/Level2/dz2/task.java
@@ -48,7 +48,22 @@ public class task {
                .reduce((integer, integer2) -> integer + integer2);
         System.out.println(output.orElse(0));
 
-
+        List<List<Integer>> lists = List.of(List.of(1, 2), List.of(3, 4, 5), List.of());
+        //Задача №2
+        // 1. Отсортировать список lists так, чтобы сначала были самые большие списки по размеру
+        // 2. Вывести на экран все элементы
+        // Ожидаемый результат: 3,4,5,1,2
+        lists.stream()
+                .sorted((e1, e2) -> e2.size() - e1.size())
+                .forEach(System.out::print);
+        System.out.println("          ");
+        //Задача №3
+        // 1. Узнать, есть ли в lists хотя бы один список, который содержит сумму всех элементов вложенного листа
+        // равную 12
+        lists.stream()
+                .filter(e -> e.size() > 0)
+                .filter(e -> e.stream().reduce(Integer::sum).get() == 12)
+                .forEach(System.out::print);
 
 
 
